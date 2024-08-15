9c4b7428cf80a3d39a602390aa97505faaee4d5b
MachariaP <walburphinehas78@gmail.com>
2024-08-15 12:55:31 +0300

    added posts to the home view. implemented a for loop to handle posts in the html templates

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,17 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Phinehas'}
-    return render_template('index.html', user=user)
+    posts = [
+            {
+                'author': {'username': 'John'},
+                'body': 'Beautiful day in Portland!'
+                },
+            {
+                'author': {'username': 'Susan'},
+                'body': 'The Avengers movie was so cool!'
+                }
+            ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 
 if __name__ == '__main__':
