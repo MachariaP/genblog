b64c6ff0d52a5923430d7e047c38ac354b358434
MachariaP <walburphinehas78@gmail.com>
2024-08-15 23:32:45 +0300

    Creating the migration repository

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -5,10 +6,49 @@ from app import db
 
 
 class User(db.Model):
-    id: so.Mapped[int] = so.mapped_column(sa.Integer, primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    """
+    Represents a user in the application.
+
+    Attributes:
+        id (int): The unique identifier for the user.
+        username (str): The username of the user.
+        email (str): The email address of the user.
+        password_hash (Optional[str]): The hashed password of the user.
+        posts (WriteOnlyMapped[Post]): The posts authored by the user.
+    """
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(
+            sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(
+            sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+            back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    """
+    Represents a post in the application.
+
+    Attributes:
+        id (int): The unique identifier for the post.
+        body (str): The content of the post.
+        timestamp (datetime): The timestamp when the post was created.
+        user_id (int): The ID of the user who authored the post.
+        author (User): The user who authored the post.
+    """
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+            index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(
+            sa.ForeignKey(User.id), index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
