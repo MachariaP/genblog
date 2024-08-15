e5ff28e93cfa57e3997a95782f585942c0fcd57c
MachariaP <walburphinehas78@gmail.com>
2024-08-15 15:35:49 +0300

    Login view function && Login link in navigation bar

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 
-class LOginForm(FlaskForm):
+class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,6 +17,18 @@ def index():
                 }
             ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    """
+    Render the login page with a login form.
+
+    Returns:
+        str: Render HTML content for the login page.
+    """
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 
 if __name__ == '__main__':
