5b440da67e51330d700089fb6d076f416860cde3
MachariaP <walburphinehas78@gmail.com>
2024-08-15 12:24:14 +0300

    Simplified the view function and offloaded the page to a HTML template

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,8 @@
+from flask import render_template
 from app import app
-
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Phinehas'}
-    return '''
-<html>
-    <head>
-        <title>Home page - Genblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
