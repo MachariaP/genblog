b6f9c070748b75816b5cfc47f28ee0f3f99787c3
MachariaP <walburphinehas78@gmail.com>
2024-08-15 15:47:31 +0300

    Receiving login credentials

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -28,6 +28,10 @@ def login():
         str: Render HTML content for the login page.
     """
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
 
