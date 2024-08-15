0a507ae306d8817fc6379e197fa70d1a611c8255
MachariaP <walburphinehas78@gmail.com>
2024-08-15 17:15:28 +0300

    Flask-SQLAlchemy and Flask-Migrate initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from config import Config
-from flask_wtf.csrf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-csrf = CSRFProtect(app)
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
