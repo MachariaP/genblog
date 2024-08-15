2f07ea2aa9ed02244ca44b412e536a8aa49ebc14
MachariaP <walburphinehas78@gmail.com>
2024-08-15 11:38:41 +0300

    complete html page from view function

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Phinehas'}
+    return '''
+<html>
+    <head>
+        <title>Home page - Genblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
