c0a6e9aeffb91d8d771be8b638db4111cbc77cd0
MachariaP <walburphinehas78@gmail.com>
2024-08-16 11:00:09 +0300

    Conditional login and logout links && Logout view function && Flask-Login user loader function

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import login, db
 from flask_login import UserMixin
 
 
@@ -27,6 +27,9 @@ class User(UserMixin, db.Model):
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
             back_populates='author')
 
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
     def set_password(self, password: str) -> None:
         """Hash and set the user's password.
 
@@ -46,8 +49,26 @@ class User(UserMixin, db.Model):
         """
         return check_password_hash(self.password_hash, password)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    """
+    Load a user from the database by user ID.
+
+    The function is used by Flask-Login to reload the user object from the
+    user ID stored in the session.
+    It is decorated with '@login.user_loader' to register
+    it as the user loader callback.
+
+    Args:
+        id (str): The user ID stored in the session.
+            Flask-Login passes this as a string.
+
+    Returns:
+        User: The User object corresponding to the given ID, or
+        None if no user is found.
+    """
+    return db.session.get(User, int(id))
 
 
 class Post(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
+from app.models import User
+import sqlalchemy as sa
+from flask_login import logout_user
 from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -22,17 +26,35 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """
-    Render the login page with a login form.
+    Handle user login.
 
     Returns:
-        str: Render HTML content for the login page.
+        Response:
+            Redirects to the index page if the user is authenticated or login
+            is successful.
+            Otherwise, renders the login template with the login form.
     """
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = db.session.scalar(
+                sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """
+    Log out the current user and redirect to the index page.
+    """
+    logout_user()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
