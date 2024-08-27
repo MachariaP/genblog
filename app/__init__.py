41083c3afbbdcbd09c5acfd3098afee872b6933a
MachariaP <walburphinehas78@gmail.com>
2024-08-27 15:32:55 +0300

    Pagination

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,18 @@
 import logging
-import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-login = LoginManager(app)
-login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-from app import routes, models, errors  # noqa: E402, F401
+login = LoginManager(app)
+login.login_view = 'login'
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -28,22 +25,21 @@ if not app.debug:
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Genblog Failure',
-            credentials=auth, secure=secure
-        )
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler(
-        'logs/genblog.log', maxBytes=10240, backupCount=10
-    )
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
     file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-    ))
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
-    app.logger.info('Genblog startup')
+    app.logger.info('Microblog startup')
+
+from app import routes, models, errors
