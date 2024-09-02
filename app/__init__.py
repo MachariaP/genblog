71c2916442ad6efed7322ce29914402b872f5e7e
MachariaP <walburphinehas78@gmail.com>
2024-09-02 09:26:44 +0300

    Add i18n and l10n support with Spanish translations

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from config import Config
 from flask_moment import Moment
+from flask_babel import Babel
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +21,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
