191f80ce741896d4a7e564f1e59914fda6785189
MachariaP <walburphinehas78@gmail.com>
2024-09-19 15:35:20 +0300

    Update Flask app to use port from environment variable

--- genblog.py
+++ genblog.py
@@ -1,3 +1,4 @@
+import os
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import create_app, db
@@ -5,8 +6,11 @@ from app.models import User, Post, Message, Notification, Task
 
 app = create_app()
 
-
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post,
             'Message': Message, 'Notification': Notification, 'Task': Task}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
